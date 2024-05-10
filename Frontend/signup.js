1ffb53f0dc5075cc2fa742b7c9c622d8e8f7acf2
MaharnabSarkar <115912203+MaharnabSarkar@users.noreply.github.com>
2024-05-11 03:33:26 +0530

    signup page setup

---
+++ Frontend/signup.js
@@ -0,0 +1,5 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add form validation and signup logic here
+    alert('Sign up successful!');
+});
